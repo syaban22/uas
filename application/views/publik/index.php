cfe5a0b6e3de932d786549c502510f69b45814fc
170411100038 <wahyupriambodo814@gmail.com>
2019-11-29 06:40:56 +0700

    Update index.php
    Diploma changed

--- application/views/publik/index.php
+++ application/views/publik/index.php
@@ -86,7 +86,7 @@
                     </div>
                     <div class="col">
                       <p class="card-text"><i class="fas fa-fw fa-user-tie mr-3"></i>Staf Senior, Supervisor</p>
-                      <p><i class="fas fa-fw fa-graduation-cap mr-3"></i>Diploma/D3, Sarjana/S1</p>
+                      <p><i class="fas fa-fw fa-graduation-cap mr-3"></i>Diploma, Sarjana/S1</p>
                     </div>
                   </div>
                   <div class="card-footer bg-transparent border-success pr-0">
